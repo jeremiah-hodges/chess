9191bc625f1b4869c59932c28dd049c8930170ac
Jeremiah <jeremiah.hodges@duke.edu>
2021-06-21 00:35:23 -0500

    gui stuff

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,22 @@
-public class Game {
-    public Game()
-    {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class Game extends JFrame {
+
+    public Game() {
+
+        JFrame test = new JFrame("test");
+        test.setVisible(true);
+
 
     }
+
+    public static void main(String[] args){
+
+        new Game();
+    }
+
+
 }
+
